0d5e0730a0b58214aea250e70af31dece77265d2
Duquerson <sotoduqueyeferson@gmail.com>
2025-03-12 21:59:35 -0300

    add funtion valide input

--- app.js
+++ app.js
@@ -3,3 +3,11 @@
 let friends = [];
 //funcion para obtener un elemento por su id
 const getElement = (id) => document.getElementById(id);
+
+//funcion para validar el input
+const inputValide = (input) => {
+  //uso de una libreria externa para evitar ataques xss
+  let cleanInput = DOMPurify.sanitize(input).trim();
+  if (!cleanInput) alert("Debes ingresar un nombre");
+  return cleanInput;
+};
