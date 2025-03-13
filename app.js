5e650f4269fea8d7aecc402acbd561d827b3bbdd
Duquerson <sotoduqueyeferson@gmail.com>
2025-03-12 21:58:04 -0300

    add funtion getElement

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 "use strict";
 let friends = [];
+//funcion para obtener un elemento por su id
+const getElement = (id) => document.getElementById(id);
